92932de6545fab3c3648ea89b37ca6878c34ba0a
monis <monis377272@gmail.com>
2019-01-16 11:19:35 +0800

    signup and login to firebase

--- app/src/main/java/com/monis/chatapp/LoginActivity.kt
+++ app/src/main/java/com/monis/chatapp/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.monis.chatapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -14,6 +16,20 @@ class LoginActivity : AppCompatActivity() {
         login_signup.setOnClickListener {
             var intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
+        }
+
+        login_button.setOnClickListener {
+            var auth = FirebaseAuth.getInstance()
+            auth.signInWithEmailAndPassword(login_email.text.toString(),login_password.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        var intent = Intent(this, UserListActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this,task.exception?.message,Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 }
--- app/src/main/java/com/monis/chatapp/SignupActivity.kt
+++ app/src/main/java/com/monis/chatapp/SignupActivity.kt
@@ -1,12 +1,33 @@
 package com.monis.chatapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        signup_button.setOnClickListener {
+            if (signup_password.text.toString() == signup_confirm.text.toString()) {
+                var auth = FirebaseAuth.getInstance()
+                auth.createUserWithEmailAndPassword(signup_email.text.toString(),signup_password.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            var intent = Intent(this, UserListActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this,task.exception?.message,Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this,"Password not match",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
