c00014964ca809a208a600be45e6a9dcc3930458
monis <monis377272@gmail.com>
2019-01-16 12:16:23 +0800

    write and read data from firebase

--- app/src/main/java/com/monis/chatapp/SignupActivity.kt
+++ app/src/main/java/com/monis/chatapp/SignupActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
@@ -19,6 +20,11 @@ class SignupActivity : AppCompatActivity() {
                 auth.createUserWithEmailAndPassword(signup_email.text.toString(),signup_password.text.toString())
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
+                            var username:String = signup_email.text.toString().substring(0,
+                                signup_email.text.toString().indexOf("@"))
+                            var dbr =FirebaseDatabase.getInstance().reference
+                            dbr.child("users").child(username).setValue("Hi")
+
                             var intent = Intent(this, UserListActivity::class.java)
                             startActivity(intent)
                         } else {
--- app/src/main/java/com/monis/chatapp/UserListActivity.kt
+++ app/src/main/java/com/monis/chatapp/UserListActivity.kt
@@ -2,11 +2,34 @@ package com.monis.chatapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_user_list.*
 
 class UserListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_list)
+
+        var dbr = FirebaseDatabase.getInstance().reference
+        var list = ArrayList<String>()
+        dbr.child("users").addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                for (u in p0.children) {
+                    list.add(u.key.toString())
+                    var adp = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, list)
+                    users_lv.adapter = adp
+                }
+            }
+        })
+
     }
 }
